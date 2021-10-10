1be672d26d446080d8f122a39b49fbffd0a4a499
MINHAJUL ARAFIN <minhajularafin13@gmail.com>
2021-10-10 19:19:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// company start 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplaySpeed:3000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+// company end
